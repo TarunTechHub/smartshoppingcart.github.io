9bbfe6af4c499b38c80163784114d46f674452fa
Tarun S <87562668+TarunTechHub@users.noreply.github.com>
2022-07-15 11:38:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-analytics.js";
+  import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +20,7 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-</script>
+  const auth = getAuth(app);
 
 const submitButton = document.getElementById("submit");
 const signupButton = document.getElementById("sign-up");
