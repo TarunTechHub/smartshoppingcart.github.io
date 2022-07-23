b24bfb00794176b532539b30784c1068ff49b575
Tarun S <87562668+TarunTechHub@users.noreply.github.com>
2022-07-23 10:54:05 +0530

    Update constants.js

--- Chat-Bot/constants.js
+++ Chat-Bot/constants.js
@@ -1,6 +1,6 @@
 // Options the user could type in
 const prompts = [
-  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["hi", "hai", "hey", "hello", "good morning", "good afternoon"],
   ["how are you", "how is life", "how are things"],
   ["what are you doing", "what is going on", "what is up"],
   ["how old are you"],
@@ -19,18 +19,39 @@ const prompts = [
   ["help me", "tell me story", "tell me joke"],
   ["ah", "yes", "ok", "okay", "nice"],
   ["bye", "good bye", "goodbye", "see you later"],
-  ["what should i eat today"],
   ["bro"],
   ["what", "why", "how", "where", "when"],
   ["no","not sure","maybe","no thanks"],
   [""],
-  ["haha","ha","lol","hehe","funny","joke"]
+  ["haha","ha","lol","hehe","funny","joke"],
+  ["thank you"],
+  ["i want product details"],
+  ["milk"],
+  ["book"],
+  ["pen"],
+  ["rice"],
+  ["soap"],
+  ["oil"],
+  ["i want milk"],
+  ["i want book"],
+  ["i want pen"],
+  ["i want rice"],
+  ["i want soap"],
+  ["i want oil"],
+  ["milk to book"],
+  ["milk to pen"],
+  ["milk to rice"],
+  ["milk to soap"],
+  ["milk to oil"],
+  ["good night"],
+  ["tell me about yourself"],
+  ["hai bot tell something about my best friend"]
 ]
 
 // Possible responses, in corresponding order
 
 const replies = [
-  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  ["Hello!", "Hi!", "Hey!", "Hi there!"],
   [
     "Fine... how are you?",
     "Pretty well, how are you?",
@@ -44,20 +65,41 @@ const replies = [
   ],
   ["I am infinite"],
   ["I am just a bot", "I am a bot. What are you?"],
-  ["The one true God, JavaScript"],
+  ["The one only our guide, Puneeth sir"],
   ["I am nameless", "I don't have a name"],
   ["I love you too", "Me too"],
   ["Have you ever felt bad?", "Glad to hear it"],
   ["Why?", "Why? You shouldn't!", "Try watching TV"],
   ["What about?", "Once upon a time..."],
-  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Thankyou", "Tell me about yourself"],
   ["Bye", "Goodbye", "See you later"],
-  ["Sushi", "Pizza"],
   ["Bro!"],
   ["Great question"],
   ["That's ok","I understand","What do you want to talk about?"],
-  ["Wellcome to smart shopping mall :("],
-  ["Haha!","Good one!"]
+  ["Welecome to smart shopping cart"],
+  ["Haha!","Good one!"],
+  ["How can i help you...!!"],
+  ["wich product details do you want"],
+  ["The price of milk is 15"],
+  ["the price of book is 20"],
+  ["the price of pen is 10"],
+  ["the rice price start from 800"],
+  ["the soap price is 70"],
+  ["the oil price is 80"],
+  ["its on track number one"],
+  ["its on track number two"],
+  ["its on track number three"],
+  ["its on track number four"],
+  ["its on number five"],
+  ["its on track number six"],
+  ["Go straight take right..."],
+  ["take right go straight take second right"],
+  ["take right go straight take sthird right"],
+  ["take right go straight take fourth right"],
+  ["take right go straight take sixth right"],
+  ["Good night sweet dreams take care"],
+  ["im bot created by team 16 and the team members are tarun, priyanka, savina and Mehantha under the guidence of puneeth sir"],
+  ["She is crazy, She is straight forward, She is realistic, She is observant, She is down to earth, She is impartial, She is trustworthy, She is not FAKE,She is as REAL as she actually is, Also, She is caring, She is your secret keeper, She is your inspiration.Yes,she is your bestfriend you are the luckiest because,as compared to the rest,you found her the best and she is Gem one"]
 ]
 
 // Random for any other user input
@@ -65,7 +107,6 @@ const replies = [
 const alternative = [
   "Same",
   "Go on...",
-  "Pardon...",
   "Try again",
   "I'm listening...",
   "I don't understand :/"
